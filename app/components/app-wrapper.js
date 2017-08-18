e680d849a0ef9da55afc520d5ca51919eb084ea4
Vishu Sharma <Vishu.Sharma@sc.com>
2017-08-18 18:55:12 +0800

    Updated template and other changes.

--- app/components/app-wrapper.js
+++ app/components/app-wrapper.js
@@ -24,7 +24,7 @@ export default Ember.Component.extend({
 					});
 
 					// Fix: Placeholder polyfill.
-					$('form').placeholder();
+					// $('form').placeholder();
 
 					// Prioritize "important" elements on mobile.
 					skel.on('+mobile -mobile', function() {
